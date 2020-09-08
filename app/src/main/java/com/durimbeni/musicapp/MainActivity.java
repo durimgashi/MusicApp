de1a530144acd48df934ba7159faca247e53d5ab
durimgashi <durimgashi97@gmail.com>
2020-09-08 23:02:31 +0200

    Filloj puna

--- app/src/main/java/com/durimbeni/musicapp/MainActivity.java
+++ app/src/main/java/com/durimbeni/musicapp/MainActivity.java
@@ -1,14 +1,35 @@
 package com.durimbeni.musicapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    private ConstraintLayout constraintLayout;
+    private AnimationDrawable animationDrawable;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        constraintLayout = (ConstraintLayout) findViewById(R.id.constraintLayout);
+        animationDrawable = (AnimationDrawable) constraintLayout.getBackground();
+        animationDrawable.setEnterFadeDuration(3000);
+        animationDrawable.setExitFadeDuration(2000);
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (animationDrawable != null && !animationDrawable.isRunning()) {
+            animationDrawable.start();
+        }
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (animationDrawable != null && animationDrawable.isRunning()) {
+            animationDrawable.stop();
+        }
     }
 }
